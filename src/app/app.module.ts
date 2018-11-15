595ccfc2b50c8898527c00d2cdcac63dbb062586
Girish Thanki <girish.thanki@gmail.com>
2018-11-15 23:12:25 +0000

    Show custom tags

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,37 +5,66 @@ import { MonacoEditorModule, NgxMonacoEditorConfig } from 'ngx-monaco-editor';
 
 import { AppComponent } from './app.component';
 
+
+
 const monacoConfig: NgxMonacoEditorConfig = {
   onMonacoLoad: () => { 
     const id = "foo.json";
-    monaco.languages.json.jsonDefaults.setDiagnosticsOptions({
-      validate: true,
-      schemas: [{
-        uri: "http://myserver/foo-schema.json",
-        fileMatch: [id],
-        schema: {
-          type: "object",
-          properties: {
-            p1: {
-              enum: [ "v1", "v2"]
+    monaco.languages.register({ id: 'mySpecialLanguage' });
+  
+    monaco.languages.setMonarchTokensProvider('mySpecialLanguage', <any>{
+      tokenizer: {
+        root: [
+          [/\[error.*/, "custom-error"],
+          [/\[notice.*/, "custom-notice"],
+          [/\[info.*/, "custom-info"],
+          [/\[[a-zA-Z 0-9:]+\]/, "custom-date"],
+        ]
+      }    
+    });
+
+    // Define a new theme that contains only rules that match this language
+    monaco.editor.defineTheme('myCoolTheme', <any>{
+      base: 'vs',
+	    inherit: false,
+	    rules: [
+		    { token: 'custom-info', foreground: '808080' },
+		    { token: 'custom-error', foreground: 'ff0000', fontStyle: 'bold' },
+		    { token: 'custom-notice', foreground: 'FFA500' },
+		    { token: 'custom-date', foreground: '008800' },
+	    ]
+    });
+
+    // Register a completion item provider for the new language
+    monaco.languages.registerCompletionItemProvider('mySpecialLanguage', {
+      provideCompletionItems: () => {
+        return [
+          {
+            label: 'simpleText',
+            kind: monaco.languages.CompletionItemKind.Text
+          }, {
+            label: 'testing',
+            kind: monaco.languages.CompletionItemKind.Keyword,
+            insertText: {
+              value: 'testing(${1:condition})'
+            }
+          },
+          {
+            label: 'ifelse',
+            kind: monaco.languages.CompletionItemKind.Snippet,
+            insertText: {
+              value: [
+                'if (${1:condition}) {',
+                '\t$0',
+                '} else {',
+                '\t',
+                '}'
+              ].join('\n')
             },
-            p2: {
-              $ref: "http://myserver/bar-schema.json"
-            }
+            documentation: 'If-Else Statement'
           }
-        }
-      },{
-        uri: "http://myserver/bar-schema.json",
-        fileMatch: [id],
-        schema: {
-          type: "object",
-          properties: {
-            q1: {
-              enum: [ "x1", "x2"]
-            }
-          }
-        }
-      }]
+        ]
+      }
     });
   } 
 };
