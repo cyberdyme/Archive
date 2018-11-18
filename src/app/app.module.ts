d12facef6f996e7ca10f1a89dd6efca816d74c9c
Girish Thanki <girish.thanki@gmail.com>
2018-11-18 23:10:38 +0000

    Allow fix format to be formatted correctly

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,13 @@ import { MonacoEditorModule, NgxMonacoEditorConfig } from 'ngx-monaco-editor';
 
 import { AppComponent } from './app.component';
 
-
-
 const monacoConfig: NgxMonacoEditorConfig = {
   onMonacoLoad: () => { 
-    const id = "foo.json";
-    monaco.languages.register({ id: 'mySpecialLanguage' });
-  
+    monaco.languages.register({
+      id: 'mySpecialLanguage'
+    });
+    
+    // Register a tokens provider for the language
     monaco.languages.setMonarchTokensProvider('mySpecialLanguage', <any>{
       tokenizer: {
         root: [
@@ -19,53 +19,104 @@ const monacoConfig: NgxMonacoEditorConfig = {
           [/\[notice.*/, "custom-notice"],
           [/\[info.*/, "custom-info"],
           [/\[[a-zA-Z 0-9:]+\]/, "custom-date"],
-        ]
-      }    
-    });
-
-    // Define a new theme that contains only rules that match this language
-    monaco.editor.defineTheme('myCoolTheme', <any>{
-      base: 'vs',
-	    inherit: false,
-	    rules: [
-		    { token: 'custom-info', foreground: '808080' },
-		    { token: 'custom-error', foreground: 'ff0000', fontStyle: 'bold' },
-		    { token: 'custom-notice', foreground: 'FFA500' },
-		    { token: 'custom-date', foreground: '008800' },
-	    ]
-    });
-
-    // Register a completion item provider for the new language
-    monaco.languages.registerCompletionItemProvider('mySpecialLanguage', {
-      provideCompletionItems: () => {
-        return [
-          {
-            label: 'simpleText',
-            kind: monaco.languages.CompletionItemKind.Text
-          }, {
-            label: 'testing',
-            kind: monaco.languages.CompletionItemKind.Keyword,
-            insertText: {
-              value: 'testing(${1:condition})'
-            }
-          },
-          {
-            label: 'ifelse',
-            kind: monaco.languages.CompletionItemKind.Snippet,
-            insertText: {
-              value: [
-                'if (${1:condition}) {',
-                '\t$0',
-                '} else {',
-                '\t',
-                '}'
-              ].join('\n')
-            },
-            documentation: 'If-Else Statement'
-          }
+          [/\|/, "custom-pipe"]
         ]
       }
     });
+    
+    // Define a new theme that constains only rules that match this language
+    monaco.editor.defineTheme('myCoolTheme', <any>{
+      base: 'vs-dark',
+      inherit: true,
+      rules: [{
+        token: 'custom-info',
+        foreground: 'FFFFFF'
+      }, {
+        token: 'custom-error',
+        foreground: 'ff0000',
+        fontStyle: 'bold'
+      }, {
+        token: 'custom-notice',
+        foreground: 'FFA500'
+      }, {
+        token: 'custom-date',
+        foreground: '008800'
+      }, {
+        token: 'custom-pipe',
+        foreground: '009933'
+      }, ]
+    });
+    
+    monaco.languages.registerDocumentFormattingEditProvider('mySpecialLanguage', {
+      provideDocumentFormattingEdits: function(model, options, token) {
+        
+        const inputSource=model.getValue().toString();
+        
+        console.log("here"+inputSource) // which is well printed
+        
+        const inputLines=inputSource.split('\n');
+        console.log("line count"+inputLines.length) // which is well printed
+        
+        let changeLocations =[];
+    
+        
+        for(var i=0; i<inputLines.length; i++) {
+            console.log("Processing:"+i+" -> "+inputLines[i]);
+            let str = inputLines[i];
+  
+            const currentLine=i+1;
+            
+            const strLength=str.length - 2;
+            for (var j = 0; j < strLength; j++) {
+              if(str.charAt(j) === '|' && str.charAt(j+1) !== '\n'){
+                console.log(`position =>${j}`);
+                
+                const currentPosition= j+2;
+                changeLocations.push({
+                  range: {
+                  startLineNumber: currentLine,
+                  startColumn: currentPosition,
+                  endLineNumber: currentLine,
+                  endColumn: currentPosition
+                  },
+                  text: '\n'
+                })
+              }
+            }
+        }
+        
+        return changeLocations;
+      }
+    })
+    
+    // Register a completion item provider for the new language
+    monaco.languages.registerCompletionItemProvider('mySpecialLanguage', {
+      provideCompletionItems: () => {
+        return [{
+          label: 'simpleText',
+          kind: monaco.languages.CompletionItemKind.Text
+        }, {
+          label: 'testing',
+          kind: monaco.languages.CompletionItemKind.Keyword,
+          insertText: {
+            value: 'testing(${1:condition})'
+          }
+        }, {
+          label: 'ifelse',
+          kind: monaco.languages.CompletionItemKind.Snippet,
+          insertText: {
+            value: [
+              'if (${1:condition}) {',
+              '\t$0',
+              '} else {',
+              '\t',
+              '}'
+            ].join('\n')
+          },
+          documentation: 'If-Else Statement'
+        }]
+      }
+    })  
   } 
 };
 
